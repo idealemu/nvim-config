d1854fe372d0ece791eee1c6f76710103d965d55
idealemu <git@spes.simplelogin.com>
2026-03-30 15:02:53 +0200

    Finally fix linting

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,40 +1,37 @@
 local mason_lspconfig = require('mason-lspconfig')
 
-local custom_servers = { "pylsp", "ruff" }
-mason_lspconfig.setup({
-  ensure_installed = {"pylsp", "ruff"},
-  handlers = {
-    -- Default handler for all servers except those in custom_servers
-    function(server_name)
-      if not vim.tbl_contains(custom_servers, server_name) then
-        vim.lsp.config(server_name, {})
-      end
-    end,
-  },
-})
-
--- PYLSP custom config
+-- 1. Configure LSP servers directly with vim.lsp.config()
+-- These configs are registered but NOT yet enabled
 vim.lsp.config('pylsp', {
   settings = {
     pylsp = {
       plugins = {
-        pyflakes = { enabled = false }, -- Ruff conflicting
-        pycodestyle = {
-            ignore = {"E501"},
-            -- maxLineLength = 100, -- E501 override
-        },
+        -- Disable all linters
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        mccabe = { enabled = false },
+        ruff = { enabled = false },  
+
+        -- Keep useful features
+        jedi_completion = { enabled = true },
+        jedi_hover = { enabled = true },
+        jedi_signature_help = { enabled = true },
       },
     },
   },
 })
 
-
--- Ruff custom config
 vim.lsp.config('ruff', {
-  settings = {
-    args = {
-      "--enable=ARG001",  -- Unused function argument
-      "--enable=F841",    -- Unused variable
+  init_options = {
+    settings = {
+      lint = {
+        select = { "ARG001", "F841" },
+      },
     },
   },
+})
+
+-- 2. Setup mason-lspconfig 
+mason_lspconfig.setup({
+  ensure_installed = { "pylsp", "ruff" },
 })
