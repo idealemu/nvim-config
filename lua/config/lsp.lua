9054d65555af9011e6f459e721135d44b74a332c
idealemu <git@spes.simplelogin.com>
2026-03-30 12:46:46 +0200

    Pylsp and cmp further config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -10,3 +10,31 @@ mason_lspconfig.setup({
     end,
   },
 })
+
+local mason_lspconfig = require('mason-lspconfig')
+
+mason_lspconfig.setup({
+  ensure_installed = {"pylsp"},
+  handlers = {
+    function(server_name)
+      -- Default handler for all servers except custom configs
+      if server_name ~= "pylsp" then
+        require('lspconfig')[server_name].setup({})
+      end
+    end,
+  },
+})
+
+-- PYLSP custom config
+vim.lsp.config('pylsp', {
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {"E501"},
+          -- maxLineLength = 100, -- E501 override
+        },
+      },
+    },
+  },
+})
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -6,6 +6,26 @@ return {
   },
   config = function()
     local cmp = require("cmp")
+    local show_completions = true
+
+    -- Toggle auto-completion
+    vim.api.nvim_create_autocmd("TextChangedI", {
+      callback = function()
+        if not show_completions then return end
+        if cmp.visible() then return end
+        cmp.complete()
+      end
+    })
+
+    -- Map <leader>c to toggle completions in normal mode
+    vim.keymap.set("n", "<leader>l", function()
+      show_completions = not show_completions
+      if show_completions then
+        vim.notify("Auto-completion enabled")
+      else
+        vim.notify("Auto-completion disabled")
+      end
+    end, { desc = "Toggle auto-completion" })
 
     cmp.setup({
       sources = cmp.config.sources({
@@ -13,18 +33,33 @@ return {
       }),
       mapping = cmp.mapping.preset.insert({
         ['<C-c>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.abort(),
+        ['<C-e>'] = cmp.mapping.abort(), -- Exit completion
       }),
       -- Show completions as you type
       completion = {
         completeopt = "menu,menuone,noinsert",
       },
+      -- Enable hover documentation
+      window = {
+        documentation = cmp.config.window.bordered(),
+      },
+      vim.keymap.set('n', 'H', vim.lsp.buf.hover, { desc = 'Show hover documentation' })
     })
 
-    vim.api.nvim_create_autocmd("TextChangedI", {
+
+    vim.o.updatetime = 500
+    -- Automatically show diagnostics on cursor hold, if no other floating window is active
+    vim.api.nvim_create_autocmd("CursorHold", {
       callback = function()
-        if cmp.visible() then return end
-        cmp.complete()
+        local ok, win = pcall(vim.api.nvim_get_option, "winid")
+        if ok and vim.api.nvim_win_get_config(win).zindex then return end
+
+        vim.diagnostic.open_float(nil, { 
+          focus = false, 
+          source = "always" 
+        })
       end
     })
   end
