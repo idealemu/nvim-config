b6c3e861d2bf8a93dd6153f983abaa206195f81c
idealemu <git@spes.simplelogin.com>
2026-04-15 13:08:15 +0200

    Added leader to keymap

--- lua/plugins/mark.lua
+++ lua/plugins/mark.lua
@@ -5,9 +5,9 @@ return {
     require('marks').setup()
 
     -- Show all marks in current buffer (location list)
-    vim.keymap.set('n', 'gm', ':MarksListBuf<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>gm', ':MarksListBuf<CR>', { noremap = true, silent = true })
     -- Show all marks across ALL buffers
-    vim.keymap.set('n', 'gM', ':MarksListAll<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>gM', ':MarksListAll<CR>', { noremap = true, silent = true })
 
   end,
 }
